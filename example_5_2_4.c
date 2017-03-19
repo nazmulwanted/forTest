ef21125adef11d2c604e5952fedb64a7647cefce
Nazmul Hossain <nazmulwanted@gmail.com>
2017-03-19 20:57:48 +0600

    Added comments
    Just added comments in the program.

--- example_5_2_4.c
+++ example_5_2_4.c
@@ -4,37 +4,37 @@
 
 int main(){
 
-    char command[25],temp[80];
-    int i,j;
+    char command[25],temp[80]; // declaring two integer arrays
+    int i,j; // two numbers for operation
 
     for(;;){
         printf("Operation? ");
-        gets(command);
+        gets(command);// getting user commands like add, subtract , multiplication, division or quit
 
-        if(!strcmp(command,"quit")) break;
+        if(!strcmp(command,"quit")) break; // if user types 'quit' then program stops
 
         printf("Enter first number: ");
         gets(temp);
-        i=atoi(temp);
+        i=atoi(temp); // converts string to integer
 
         printf("Enter second number: ");
         gets(temp);
-        j=atoi(temp);
+        j=atoi(temp); // converts string to integer
 
         if(!strcmp(command,"add")){
-            printf("%d\n",i+j);
+            printf("%d\n",i+j); // if user commands 'add' then does addition
         }
         else if(!strcmp(command,"subtract")){
-            printf("%d\n",i-j);
+            printf("%d\n",i-j); // if user commands 'subtract' then does subtraction
         }
         else if(!strcmp(command,"multiply")){
-            printf("%d\n",i*j);
+            printf("%d\n",i*j); // if user commands 'multiply' then does multiplication
         }
         else if(!strcmp(command,"divide")){
-            printf("%d\n",i/j);
+            printf("%d\n",i/j); // if user commands 'divide' then does division
         }
         else {
-            printf("Unknown command!\n");
+            printf("Unknown command!\n"); // if nothing matches then shows 'unknown command' message
         }
     }
 
